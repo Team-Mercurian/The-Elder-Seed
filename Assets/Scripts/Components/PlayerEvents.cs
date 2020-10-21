5bef9b98118c66cb8af4cf5da098e7d56972b930
Extelen <49574554+Extelen@users.noreply.github.com>
2020-10-21 01:21:31 -0300

    Implemented Player Events for Colliders.

---
+++ Assets/Scripts/Components/PlayerEvents.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEvents : MonoBehaviour {
+	
+    //Establecer variables.
+		
+        //Establecer estructuras.
+		
+        //Establecer enumeradores.
+		
+        //Establecer variables estaticas.
+		
+            //Publicas.
+			
+            //Privadas
+			
+        //Establecer variables.
+		
+            //Publicas.
+			
+            //Privadas.
+			
+			
+    //Funciones
+		
+        //Funciones de MonoBehaviour
+        private void OnTriggerEnter(Collider collider) {
+			
+            if (collider.CompareTag("Forest Path")) {
+
+                SceneController.GetSingleton().LoadScene(SceneController.Scenes.Forest);
+                }
+
+            if (collider.CompareTag("Forest Passage")) {
+
+                Vector2Int m_pos = collider.GetComponent<ForestPassageController>().GetPositionToMove();
+                DataSystem.GetSingleton().SetActualRoom(m_pos);
+                
+                SceneController.GetSingleton().LoadScene(SceneController.Scenes.Forest);
+                }
+            }
+		
+        //Funciones privadas.
+		
+        //Funciones publicas.
+		
+        //Funciones heredadas.
+		
+        //Funciones ha heredar.
+		
+        //Corotinas.
+		
+        }
