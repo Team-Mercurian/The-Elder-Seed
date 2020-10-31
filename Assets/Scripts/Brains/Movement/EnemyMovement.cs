72a679211f07bf6ad44dbdcb890266b5115f905b
Extelen <49574554+Extelen@users.noreply.github.com>
2020-10-31 02:07:10 -0300

    Implemented Slime (Prototype).

---
+++ Assets/Scripts/Brains/Movement/EnemyMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyMovement : JumpingCharacter {
+	
+    //Establecer variables.
+		
+        //Establecer estructuras.
+		
+        //Establecer enumeradores.
+		
+        //Establecer variables estaticas.
+		
+            //Publicas.
+			
+            //Privadas
+			
+        //Establecer variables.
+		
+            //Publicas.
+			
+            //Privadas.
+			
+			
+    //Funciones
+		
+        //Funciones de MonoBehaviour.
+
+        //Funciones privadas.
+		
+        //Funciones publicas.
+		protected void ConstantJump(float minSecs, float maxSecs) {
+
+            StartCoroutine(ConstantJumpCoroutine(minSecs, maxSecs));
+            }
+
+        //Funciones heredadas.
+		
+        //Funciones ha heredar.
+		
+        //Corotinas.
+        private IEnumerator ConstantJumpCoroutine(float minSecs, float maxSecs) {
+            
+            while(true) {
+
+                Jump();
+                yield return new WaitForSeconds(Random.Range(minSecs, maxSecs));
+                }
+            }
+        }
