68d570dcaa03b73537e7473bdb9cf017845fdec3
Extelen <49574554+Extelen@users.noreply.github.com>
2020-10-31 01:28:29 -0300

    Implemented Health System.

---
+++ Assets/Scripts/Components/Health/PlayerHealth.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : HealthBehaviour {
+	
+    //Establecer variables.
+		
+        //Establecer estructuras.
+		
+        //Establecer enumeradores.
+		
+        //Establecer variables estaticas.
+		
+            //Publicas.
+			
+            //Privadas
+			
+        //Establecer variables.
+		
+            //Publicas.
+			
+            //Privadas.
+			
+			
+    //Funciones
+		
+        //Funciones de MonoBehaviour
+		
+        //Funciones privadas.
+		
+        //Funciones publicas.
+		
+        //Funciones heredadas.
+		protected override int SetActualHealth() {
+
+            return m_health;
+            }
+		protected override void Dead() {
+            
+            Debug.Log("Dead is not implemented yet.");
+            }
+
+        //Funciones ha heredar.
+		
+        //Corotinas.
+		
+        }
